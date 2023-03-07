eb502b5ec84b3725154253944973eeddb4cf90ad
Ruben Grillaert <ruben.grillaert@delaware.pro>
2023-03-07 11:21:04 +0100

    documentation guide + ps1 template

---
+++ Template/template-script-powershell.ps1
@@ -0,0 +1,11 @@
+﻿#Naming convention (can be removed if applied): template-script-powershell.ps1
+
+#Description
+##This is a template script that can be used for documentation.
+
+#Attention points
+##Try to execute the script on your own dev tenant before using somewhere else.
+
+#Prerequisites
+##Install PnP.PowerShell and add yourself as admin of the SharePoint site
+
