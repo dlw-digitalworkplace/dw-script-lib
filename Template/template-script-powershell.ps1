d9ea7871fe8215a2c1469f4044e6d34458a0c77d
Ruben Grillaert <ruben.grillaert@delaware.pro>
2023-03-21 10:50:14 +0100

    Update script template + create Utils folder

--- Template/template-script-powershell.ps1
+++ Template/template-script-powershell.ps1
@@ -1,11 +1,14 @@
-﻿#Naming convention (can be removed if applied): template-script-powershell.ps1
+﻿# Naming convention (can be removed if applied): template-script-powershell.ps1
 
-#Description
-##This is a template script that can be used for documentation.
+# Description
+## This is a template script that can be used for documentation.
 
-#Attention points
-##Try to execute the script on your own dev tenant before using somewhere else.
+# Attention points
+## Try to execute the script on your own dev tenant before using somewhere else.
+## Run as admin.
+## ...
 
-#Prerequisites
-##Install PnP.PowerShell and add yourself as admin of the SharePoint site
+# Prerequisites
+## Install PnP.PowerShell and add yourself as admin of the SharePoint site
 
+# Script
