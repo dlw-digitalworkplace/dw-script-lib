08436f49f9a423cadb793b5050bc9bd14eb74ec8
Robin Agten <robin.agten@gmail.com>
2023-10-26 07:56:23 +0200

    New Script: Assign-FieldCustomizerExtension

---
+++ PowerShell/Assign-FieldCustomizerExtension/Assign-FieldCustomizerExtension.ps1
@@ -0,0 +1,41 @@
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$siteCollectionUrl,
+  [Parameter(Mandatory = $true)]
+  [string]$fieldInternalName,
+  [Parameter(Mandatory = $true)]
+  [string]$clientSideComponentId,
+  [Parameter(Mandatory = $false)]
+  [string]$listName, 
+  [Parameter(Mandatory = $false)]
+  [switch]$updateExistingLists
+)
+
+# Install PnP.PowerShell v1.22 if not already installed
+if (-not (Get-Module -Name "PnP.PowerShell" -ListAvailable)) {
+  Install-Module -Name "PnP.PowerShell@1.22" -Scope CurrentUser -Force
+}
+
+# Connect to the site collection
+Connect-PnPOnline -Url $siteCollectionUrl -Interactive
+
+# Find the specified field
+$field = $null
+if ($listName -eq $null -or $listName -eq "") {
+  $field = Get-PnPField -Identity $fieldInternalName
+}
+else {
+  $field = Get-PnPField -Identity $fieldInternalName -List $listName
+}
+
+# Associate a field customizer
+if ($null -eq $field) {
+  Write-Host -ForegroundColor Red "Field '$fieldInternalName' not found"
+}
+else {
+  Set-PnPField -Identity $field -Values @{ ClientSideComponentId = [GUID]$clientSideComponentId } -updateExistingLists:$UpdateExistingLists
+  Write-Host -ForegroundColor Green "Field '$fieldInternalName' updated"
+}
+
+# Disconnect from the site collection
+Disconnect-PnPOnline
